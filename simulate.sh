fd5481bc6695ee3042b9b83cda0f940c9de0c188
Rohan Gupta <Rohan Gupta>
2021-03-01 17:39:29 +0800

    Added simulation script

---
+++ simulate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python source/generator.py 1000
+python source/simulator.py 'data/scraper_status.json' 'data/cheetah_status_OFF.json' 'OFF'
+echo "Delaying simulator by $1 seconds..."
+sleep $1
+python source/simulator.py 'data/scraper_status.json' 'data/cheetah_status_ON.json' 'ON'
