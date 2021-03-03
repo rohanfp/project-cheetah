38ab8e60574448a48078f5dfe78dbdf8e29b23d4
Rohan Gupta <Rohan Gupta>
2021-03-03 09:43:58 +0800

    Updated simulate.sh

--- simulate.sh
+++ simulate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+mkdir data
 python source/generator.py 1000
 python source/simulator.py 'data/scraper_status.json' 'data/cheetah_status_OFF.json' 'OFF'
 echo "Delaying simulator by $1 seconds..."
