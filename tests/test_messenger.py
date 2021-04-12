47531117154efc1e05267378196da53a47eb9fa6
Rohan Gupta <rohan.gupta@foodpanda.com>
2021-04-12 12:04:09 +0800

    Updated tests for messenger

--- tests/test_messenger.py
+++ tests/test_messenger.py
@@ -18,14 +18,14 @@ def mock_env_2(monkeypatch):
 
 class TestMessenger:
     def test_messenger_incoming(self, mock_env_1):
-        event = json.load(open('tests/test_data/mock_incoming_messenger.json', 'r'))[0]
+        event = json.load(open('tests/test_data/mock_messenger_incoming.json', 'r'))[0]
         context = {}
         incoming.translate_message(event, context)
         assert True
 
 
     def test_messenger_outgoing(self, mock_env_2):
-        event = json.load(open('tests/test_data/mock_outgoing_messenger.json', 'r'))
+        event = json.load(open('tests/test_data/mock_messenger_outgoing.json', 'r'))
         context = {}
         outgoing.translate_message(event, context)
         assert True
