5ff2c24b18b9562360af101801232b5897a3fe37
Rohan Gupta <rohan.gupta@foodpanda.com>
2021-04-12 11:44:19 +0800

    Added logic for messenger

--- source/messenger/outgoing.py
+++ source/messenger/outgoing.py
@@ -1,4 +1,4 @@
-from .translator.translatorsfactory import TranslatorsFactory
+from ..translator.translatorsfactory import TranslatorsFactory
 
 import os
 
@@ -7,10 +7,10 @@ def translate_message(event, context):
     # Universal message format translator to keep cheetah aws service agnostic
     translator = TranslatorsFactory().create_translator(channel_name=os.getenv("CHANNEL_NAME"))
     translated_message = translator.translate(event)
-    cheetah(**translated_message)
+    outgoing_messenger(**translated_message)
     return translated_message
 
 
-def cheetah(signal_name, ip_address, scraper_type, service_name=None, time=None):
-    # Incoming message forwarder
+def outgoing_messenger(*args, **kwargs):
+    # Outgoing message forwarder
     pass
