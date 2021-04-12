ceee63721c52235c41044b23c34c312c4db3b494
Rohan Gupta <rohan.gupta@foodpanda.com>
2021-04-12 12:02:40 +0800

    Updated tests for translator

--- tests/test_translator.py
+++ tests/test_translator.py
@@ -7,8 +7,8 @@ import pytest
 class TestTranslator:
     def test_translator_sqs(self):
         translator = TranslatorsFactory().create_translator(channel_name="sqs")
-        message_0 = json.load(open('tests/test_data/mock_event_data_sqs.json', 'r'))[0]
-        message_1 = json.load(open('tests/test_data/mock_event_data_sqs.json', 'r'))[1]
+        message_0 = json.load(open('tests/test_data/mock_translator_data_sqs.json', 'r'))[0]
+        message_1 = json.load(open('tests/test_data/mock_translator_data_sqs.json', 'r'))[1]
         translated_message_0 = translator.translate(message_0)
         translated_message_1 = translator.translate(message_1)
         assert set(translated_message_0) == {"signal_name", "ip_address", "scraper_type", "service_name", "time"}
@@ -16,6 +16,6 @@ class TestTranslator:
 
     def test_translator_sns(self):
         translator = TranslatorsFactory().create_translator(channel_name="sns")
-        message = json.load(open('tests/test_data/mock_event_data_sns.json', 'r'))
+        message = json.load(open('tests/test_data/mock_translator_data_sns.json', 'r'))
         translated_message = translator.translate(message)
         assert set(translated_message) == {"signal_name", "ip_address", "scraper_type", "service_name", "time"}
